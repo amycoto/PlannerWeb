d97177f41497a61f57730d58aae4b90e71f41e7e
Orvil Escalante <orvilescalante@Mac.lan>
2025-11-20 00:13:36 -0600

    WIP: Implementing local storage functionality for sessions with MVC architecture

--- src/App.tsx
+++ src/App.tsx
@@ -2,20 +2,34 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { SessionModel } from './models/SessionModel.ts'
 
 function App() {
   const [count, setCount] = useState(0)
 
+  try {
+    console.log("Initial Sessions:", SessionModel.getAllSessions());
+
+    const newSession = SessionModel.createSession({
+      title: "Test Session",
+      subject: "CS123",
+      date:"2025-11-20",
+      startTime:"10:00",
+      duration:60
+    });
+    console.log("Created Session:", newSession);
+
+    console.log("Sessions after creation:", SessionModel.getAllSessions());
+
+    SessionModel.updateSession(newSession.id, { completed: true });
+    console.log("Updaed Completed Session:", SessionModel.getAllSessions());
+
+  } catch(e){
+    console.error(e);
+  }
+
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
